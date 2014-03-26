ef547a8c061e9e4c65f74f56fa4691f552aed649
Zach Lucas <zach.lucas@smallplanet.com>
2014-03-26 17:23:04 -0400

    Sends and receives successfully

--- ParseStarterProject/ParseStarterProjectViewController.h
+++ ParseStarterProject/ParseStarterProjectViewController.h
@@ -2,5 +2,7 @@
 @property (strong, nonatomic) IBOutlet UITextField *messageText;
 @property (strong, nonatomic) IBOutlet UILabel *mainLabel;
 @property (strong, nonatomic) IBOutlet UILabel *sentBy;
+@property (strong, nonatomic) IBOutlet UILabel *sentHeader;
+@property (strong, nonatomic) IBOutlet UILabel *charCounter;
 
 @end
--- ParseStarterProject/ParseStarterProjectViewController.m
+++ ParseStarterProject/ParseStarterProjectViewController.m
@@ -42,8 +42,11 @@
             NSLog(@"Random message: %@",[objects[randomIndex] objectForKey:@"message"]);
             NSLog(@"Random id: %@",[objects[randomIndex] objectId]);
             
+            _sentHeader.text = @"Sent!  Here's your response:";
             _mainLabel.text = [objects[randomIndex] objectForKey:@"message"];
             _sentBy.text = [@"Sent by: " stringByAppendingString:[objects[randomIndex] objectForKey:@"name"]];
+            _mainLabel.backgroundColor = [self colorWithHexString:@"dc99b8"];
+            _mainLabel.textColor = [UIColor whiteColor];
             
             NSString *objectId = [objects[randomIndex] objectId];
             
@@ -67,6 +70,12 @@
     
 
 }
+- (IBAction)keyPressed:(id)sender {
+    NSMutableString* aString = [NSMutableString stringWithFormat:@"("];
+    [aString appendFormat:@"%d", _messageText.text.length];
+    [aString appendString:@"/50)"];
+    _charCounter.text = aString;
+}
 
 - (void) sendMessage{
     PFObject *testObject = [PFObject objectWithClassName:@"Message"];
@@ -76,6 +85,42 @@
     [testObject saveInBackground];
     
     NSLog(@"Data Sending: %@",self.messageText.text);
+}
+
+-(UIColor*)colorWithHexString:(NSString*)hex
+{
+    NSString *cString = [[hex stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    
+    // String should be 6 or 8 characters
+    if ([cString length] < 6) return [UIColor grayColor];
+    
+    // strip 0X if it appears
+    if ([cString hasPrefix:@"0X"]) cString = [cString substringFromIndex:2];
+    
+    if ([cString length] != 6) return  [UIColor grayColor];
+    
+    // Separate into r, g, b substrings
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    NSString *rString = [cString substringWithRange:range];
+    
+    range.location = 2;
+    NSString *gString = [cString substringWithRange:range];
+    
+    range.location = 4;
+    NSString *bString = [cString substringWithRange:range];
+    
+    // Scan values
+    unsigned int r, g, b;
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+    
+    return [UIColor colorWithRed:((float) r / 255.0f)
+                           green:((float) g / 255.0f)
+                            blue:((float) b / 255.0f)
+                           alpha:1.0f];
 }
 
 @end
